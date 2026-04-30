0a029cfba8b0ec7790a21676d89b85405117b129
kyzhang <robert_kaiyuan@outlook.com>
2026-04-30 20:25:32 +0800

    4/30

--- 86.分隔链表.cpp
+++ 86.分隔链表.cpp
@@ -18,7 +18,28 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        
+       ListNode dummy1(-1), *p1 = &dummy1;
+       ListNode dummy2(-1), *p2 = &dummy2;
+
+       ListNode* p = head;
+
+       while (p != nullptr) {
+        if (p->val >= x) {
+          p2->next = p;
+          p2 = p2->next;
+        } else {
+          p1->next = p;
+          p1 = p1->next;
+        }
+
+        ListNode* temp = p->next;
+        p->next = nullptr;
+        p = temp;
+       }
+
+       p1->next = dummy2.next;
+
+       return dummy1.next;
     }
 };
 // @lc code=end
